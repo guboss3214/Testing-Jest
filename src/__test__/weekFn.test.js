ae65605dec4ab47dc7c495c805370b1bebb85516
Yur@Fiv3 <uraporskalo@gmail.com>
2024-11-10 17:54:22 +0200

    Testing Jest Homework

---
+++ src/__test__/weekFn.test.js
@@ -0,0 +1,17 @@
+import { weekFn } from '../weekFn';
+
+describe('Checking the week number', function(){
+    it('Should return correct day of the week', function(){
+        expect(weekFn(1)).toBe('Понеділок');
+        expect(weekFn(2)).toBe('Вівторок');
+        expect(weekFn(3)).toBe('Середа');
+        expect(weekFn(4)).toBe('Четвер');
+        expect(weekFn(5)).toBe('П\'ятниця');
+        expect(weekFn(6)).toBe('Субота');
+        expect(weekFn(7)).toBe('Неділя');
+        expect(weekFn(8)).toBe(null);
+        expect(weekFn('2')).toBe(null);
+        expect(weekFn(undefined)).toBe(null);
+        expect(weekFn(null)).toBe(null);
+    });
+});
